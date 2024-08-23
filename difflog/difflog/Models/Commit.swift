ed0141ddb60150e5ddcaac64b5437458adbd253c
Hosi121 <takuyakataiwa@icloud.com>
2024-08-23 23:07:30 +0900

    docs: gitignoreを追加

--- difflog/difflog/Models/Commit.swift
+++ difflog/difflog/Models/Commit.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class Commit {
+    var id: UUID
+    var timestamp: Date
+    @Relationship(deleteRule: .cascade) var diff: Diff?
+    @Relationship(deleteRule: .cascade) var location: Location
+    
+    init(id: UUID = UUID(), timestamp: Date = Date(), diff: Diff? = nil, location: Location? = nil) {
+        self.id = id
+        self.timestamp = timestamp
+        self.diff = diff
+        self.location = location ?? Location()
+    }
+}
--- difflog/difflog/Models/ObjectDiff.swift
+++ difflog/difflog/Models/ObjectDiff.swift
@@ -5,4 +5,20 @@
 //  Created by GTProgrammer on 2024/08/23.
 //
 
+import SwiftData
 import Foundation
+
+@Model
+final class ObjectModel {
+    var id: UUID
+    var name: String
+    var type: String
+    var count: Int
+    
+    init(id: UUID = UUID(), name: String, type: String, count: Int) {
+        self.id = id
+        self.name = name
+        self.type = type
+        self.count = count
+    }
+}
