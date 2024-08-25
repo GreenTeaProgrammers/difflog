5b3b52efd43b53ccceffa08ccb416b66edc018b5
h1ne <henryharlay@gmail.com>
2024-08-25 18:15:00 +0900

    feat: Get environment variables from .env

--- machine-learning/cml_export.py
+++ machine-learning/cml_export.py
@@ -1,5 +1,11 @@
 from ultralytics import YOLO
 
+# imports for reading .env
+import os
+from dotenv import load_dotenv, dotenv_values 
+# load .env file
+load_dotenv() 
+
 # Export the model
-model = YOLO("yolov8n.pt")
+model = YOLO(os.getenv("TRAINED_MODEL"))
 model.export(format="coreml")
